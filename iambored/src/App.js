1c0687b54bf9f6fdd5f2b32db69aa24dde7e4afb
benjaminbanwart <BenjiBanwart@Gmail.com>
2023-04-21 18:51:42 -0500

    Added navbar boilerplate

--- iambored/src/App.js
+++ iambored/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import TopBar from './components/TopBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <TopBar/>
       </header>
     </div>
   );
---
+++ iambored/src/components/TopBar.js
@@ -0,0 +1,50 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Button from 'react-bootstrap/Button'
+
+function TopBar() {
+    return (
+        <Nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <Button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </Button>
+                <div class="collapse navbar-collapse" id="navbarColor03">
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Home
+                                <span class="visually-hidden">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Separated link</a>
+                            </div>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-sm-2" type="search" placeholder="Search"/>
+                        <Button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</Button>
+                    </form>
+                </div>
+            </div>
+        </Nav>
+    )
+}
+
+export default TopBar;
