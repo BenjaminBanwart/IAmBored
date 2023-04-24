96aad9adb1cfbf0842e156288ec7012405349900
benjaminbanwart <BenjiBanwart@Gmail.com>
2023-04-23 22:38:58 -0500

    Installed Axios package, added useStates in Home page

--- iambored/src/components/Home.js
+++ iambored/src/components/Home.js
@@ -2,6 +2,9 @@ import { useState } from 'react';
 
 export default function Home () {
     let [thingsToDo, updateThingsToDo] = useState([])
+    let [participants, updateParticipants] = useState(1)
+    let [activityType, updateActivityType] = useState('')
+    let [activityPrice, updateActivityPrice] = useState(0)
 
     return (
         <div>
